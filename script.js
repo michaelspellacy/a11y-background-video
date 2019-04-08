983c91372c6d9a76fc873b2e872bd84f6a58847a
Spellacy, Michael <spell@spellacy.net>
2019-04-08 17:30:44 -0400

    test

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ if(document.getElementById("hero-banner-video")) {
 	function getCookie(name) {
 
 		var re = new RegExp(name + "=([^;]+)");
-		var value = re.exec(document.cookie);
-		return (value != null) ? unescape(value[1]) : null;
+    var value = re.exec(document.cookie);
+    return (value !== null) ? unescape(value[1]):null;
 
 	}
 

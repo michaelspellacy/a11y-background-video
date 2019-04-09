6b1828fbe5fb572f97463db9cb0c11ebde4e6298
Spellacy, Michael <spell@spellacy.net>
2019-04-09 18:06:24 -0400

    test me

--- script.js
+++ script.js
@@ -1,137 +1,143 @@
 // Accessible Background Video
 // Developer: Michael "Spell" Spellacy, Developer: Michael "Spell" Spellacy. Twitter: @spellacy, GitHub: michaelspellacy
 
-var heroBanner = document.getElementById("hero-banner");
+(function() {
 
-if(heroBanner) {
+	var heroBanner = document.getElementById("hero-banner");
 
-	function getCookie(name) {
+	if(heroBanner) {
 
-		var re = new RegExp(name + "=([^;]+)");
-    var value = re.exec(document.cookie);
-    return (value !== null) ? unescape(value[1]):null;
+		// Get Cookie
 
-	}
+		function getCookie(name) {
 
-	// Create Video Element
-
-	var newVideo = document.createElement("video");
-	newVideo.id = "hero-banner-video";
-
-	heroBanner.appendChild(newVideo);
-
-	var heroBannerPaused = getCookie("heroBannerPaused");
-	var heroBannerVideo = document.getElementById("hero-banner-video");
-	var heroBannerMedia = heroBanner.getAttribute("data-banner-media");
-	var heroBannerDesktop = heroBanner.getAttribute("data-banner-desktop");
-	var heroBannerMobile = heroBanner.getAttribute("data-banner-mobile");
-	var heroBannerPlay = "Play Video";
-	var heroBannerPause = "Pause Video";
-	var heroBannerState = "paused";
-
-	function pauseVideo() {
-
-		heroBannerVideo.pause();
-		heroBanner.classList.add(heroBannerState);
-		button.setAttribute("aria-label", heroBannerPlay);
-
-	}
-
-	function playVideo() {
-
-		heroBannerVideo.play();
-		heroBanner.classList.remove(heroBannerState);
-		button.setAttribute("aria-label", heroBannerPause);
-
-	}
-
-	function viewPortWidth(mediaQuery) {
-
-		if (mediaQuery.matches) {
-
-			heroBannerVideo.setAttribute("src", heroBannerDesktop);
-
-		} else {
-
-			heroBannerVideo.setAttribute("src", heroBannerMobile);
+			var re = new RegExp(name + "=([^;]+)");
+	    var value = re.exec(document.cookie);
+	    return (value !== null) ? unescape(value[1]):null;
 
 		}
 
-		heroBannerVideo.loop = true;
-		heroBannerVideo.muted = true;
-		heroBannerVideo.playsinline = true;
+		// Create Video Element
 
-		if(heroBanner.classList.contains(heroBannerState)) {
+		var newVideo = document.createElement("video");
+		newVideo.id = "hero-banner-video";
+
+		heroBanner.appendChild(newVideo);
+
+		var heroBannerPaused = getCookie("heroBannerPaused");
+		var heroBannerVideo = document.getElementById("hero-banner-video");
+		var heroBannerMedia = heroBanner.getAttribute("data-banner-media");
+		var heroBannerDesktop = heroBanner.getAttribute("data-banner-desktop");
+		var heroBannerMobile = heroBanner.getAttribute("data-banner-mobile");
+		var heroBannerPlay = "Play Video";
+		var heroBannerPause = "Pause Video";
+		var heroBannerState = "paused";
+
+		function pauseVideo() {
 
 			heroBannerVideo.pause();
+			heroBanner.classList.add(heroBannerState);
+			button.setAttribute("aria-label", heroBannerPlay);
 
-		} else {
+		}
+
+		function playVideo() {
 
 			heroBannerVideo.play();
+			heroBanner.classList.remove(heroBannerState);
+			button.setAttribute("aria-label", heroBannerPause);
 
 		}
 
-	}
+		function viewPortWidth(mediaQuery) {
 
-	const mediaQuery = matchMedia(heroBannerMedia);
-	mediaQuery.addListener(viewPortWidth);
-	viewPortWidth(mediaQuery);
+			if (mediaQuery.matches) {
 
-	// Check for prefers-reduced-motion
+				heroBannerVideo.setAttribute("src", heroBannerDesktop);
 
-	function viewportMotion(motionQuery){
+			} else {
 
-		if (motionQuery.matches) {
+				heroBannerVideo.setAttribute("src", heroBannerMobile);
+
+			}
+
+			heroBannerVideo.loop = true;
+			heroBannerVideo.muted = true;
+			heroBannerVideo.playsinline = true;
+
+			if(heroBanner.classList.contains(heroBannerState)) {
+
+				heroBannerVideo.pause();
+
+			} else {
+
+				heroBannerVideo.play();
+
+			}
+
+		}
+
+		const mediaQuery = matchMedia(heroBannerMedia);
+		mediaQuery.addListener(viewPortWidth);
+		viewPortWidth(mediaQuery);
+
+		// Check for prefers-reduced-motion
+
+		function viewportMotion(motionQuery){
+
+			if (motionQuery.matches) {
+
+				pauseVideo();
+
+			}
+
+		}
+
+		const motionQuery = matchMedia("(prefers-reduced-motion: reduce)");
+		motionQuery.addListener(viewportMotion);
+		viewportMotion(motionQuery);
+
+		// Create Play/Pause Button
+
+		var button = document.createElement("button");
+		button.id = "hero-banner-button";
+
+		// Check Cookie. If set to true, pause video.
+
+		if(heroBannerPaused !== null) {
 
 			pauseVideo();
-
-		}
-
-	}
-
-	const motionQuery = matchMedia("(prefers-reduced-motion: reduce)");
-	motionQuery.addListener(viewportMotion);
-	viewportMotion(motionQuery);
-
-	// Create Play/Pause Button
-
-	var button = document.createElement("button");
-	button.id = "hero-banner-button";
-
-	// Check Cookie. If set to true, pause video.
-
-	if(heroBannerPaused !== null) {
-
-		pauseVideo();
-
-	} else {
-
-		button.setAttribute("aria-label", heroBannerPause);
-
-	}
-
-	// Add Button
-
-	heroBanner.appendChild(button);
-
-	// Play/Pause Button Event
-
-	button.onclick = function(){
-
-		if (heroBanner.classList.contains(heroBannerState)) {
-
-			playVideo();
-
-			document.cookie = "heroBannerPaused=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
 		} else {
 
-			pauseVideo();
+			button.setAttribute("aria-label", heroBannerPause);
 
-			document.cookie = "heroBannerPaused=true; Secure; path=/";
+		}
+
+		// Add Button
+
+		heroBanner.appendChild(button);
+
+		// Play/Pause Button Event
+
+		button.onclick = function(){
+
+			if (heroBanner.classList.contains(heroBannerState)) {
+
+				playVideo();
+
+				document.cookie = "heroBannerPaused=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+			} else {
+
+				pauseVideo();
+
+				document.cookie = "heroBannerPaused=true; Secure; path=/";
+
+			}
 
 		}
 
 	}
 
-}
+})();
